1bb6194056e41e5cea92a485a131536a5b1a61b3
Shaine Ransford <159803141+ShaineR19@users.noreply.github.com>
2025-05-06 20:21:32 +0000

    90 degrees

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ elif choice == "FTE by Division":
             ax.set_title(f"Top 10 Sections by Total FTE in {division_input}")
             ax.set_xlabel("Section Name")
             ax.set_ylabel("Total FTE")
-            ax.tick_params(axis='x', rotation=45)
+            ax.tick_params(axis='x', rotation=90)
 
             st.pyplot(fig)
 
