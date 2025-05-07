6fe2bd77cf6b273b4eff90f52b70dcae0beea175
Shaine Ransford <159803141+ShaineR19@users.noreply.github.com>
2025-05-07 22:04:10 +0000

    blue green reversed

--- app.py
+++ app.py
@@ -747,7 +747,7 @@ elif choice == "FTE per Course":
                 sns.barplot(data=plot_df.head(10), 
                 x='Sec Name', 
                 y='Generated FTE Float', 
-                ax=ax, palette='GnBu'
+                ax=ax, palette='BuGn_r'
                 )
 
                 # Label and style
